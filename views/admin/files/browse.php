f2910013817ae53f0bde3e1cab358e77cd1c9589
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-11-24 16:33:07 +0100

    Update browse.php

--- views/admin/files/browse.php
+++ views/admin/files/browse.php
@@ -19,8 +19,7 @@
 							array(
 								__('Filename') => 'filename',
 								__('Date Added') => 'added',
-								__('Item') => 'item_id',
-								__('Owner') => 'owner_id'
+								__('Item') => 'item_id'
 							),
 							array('link_tag' => 'th scope="col"', 'list_tag' => '')
 						);
@@ -31,7 +30,6 @@
 				<?php $key = 0; ?>
 				<?php $showOriginalFilename = (bool)get_option(hide_files_show_original_filename); ?>
 				<?php foreach ($files as $file): ?>
-					<?php $item = get_record_by_id('item', $file->item_id); ?>
 					<tr class="<?php echo (++$key%2 == 1 ? 'odd' : 'even'); ?>">
 						<td>
 							<?php
@@ -46,12 +44,8 @@
 						</td>
 						<td>
 							<?php
-								echo '<a href="' . url('/items/show/' . $file->item_id) . '">' . metadata($item, 'display_title', array('no_escape' => true, 'snippet' => 50)) . '</a>';
-							?>
-						</td>
-						<td>
-							<?php
-								echo strip_formatting(metadata($item->getOwner(), 'name'));
+								$item = get_record_by_id('item', $file->item_id);
+								echo '<a href="' . url('/items/show/' . $file->item_id) . '">' . metadata($item, 'display_title', array('no_escape' => true)) . '</a>';
 							?>
 						</td>
 					</tr>
