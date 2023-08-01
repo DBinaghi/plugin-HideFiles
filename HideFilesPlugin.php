c42fe4b6a661b1980ad1ce05b346828988fc5db8
Marcin Wilkowski <m@wilkowski.org>
2023-08-01 17:30:42 +0200

    Update HideFilesPlugin.php
    update api/files to hide the file URIs and original name if !public

--- HideFilesPlugin.php
+++ HideFilesPlugin.php
@@ -354,3 +354,101 @@ class HideFilesPlugin extends Omeka_Plugin_AbstractPlugin
 		return in_array($columnName, $result);
 	}
 }
+
+// update api/files 
+// hide file uris and original name if !public		
+
+class Api_File extends Omeka_Record_Api_AbstractRecordAdapter
+{
+    /**
+     * Get the REST API representation for a file.
+     * 
+     * @param File $record
+     * @return array
+     */
+    public function getRepresentation(Omeka_Record_AbstractRecord $record)
+    {	
+
+		$is_public = $record->public;
+
+		if ($is_public) {
+
+			$representation = array(
+				'id' => $record->id,
+				'public' => $record->public,
+				'url' => self::getResourceUrl("/files/{$record->id}"),
+				'file_urls' => array(
+					'original' => $record->getWebPath(),
+					'fullsize' => $record->has_derivative_image ? $record->getWebPath('fullsize') : null,
+					'thumbnail' => $record->has_derivative_image ? $record->getWebPath('thumbnail') : null,
+					'square_thumbnail' => $record->has_derivative_image ? $record->getWebPath('square_thumbnail') : null,
+				),
+				'added' => self::getDate($record->added),
+				'modified' => self::getDate($record->modified),
+				'filename' => $record->filename,
+				'authentication' => $record->authentication,
+				'has_derivative_image' => (bool) $record->has_derivative_image,
+				'mime_type' => $record->mime_type,
+				'order' => $record->order,
+				'original_filename' => $record->original_filename,
+				'size' => $record->size,
+				'stored' => (bool) $record->stored,
+				'type_os' => $record->type_os,
+				'metadata' => json_decode($record->metadata, true),
+			);
+
+		} else {
+
+			$representation = array(
+				'id' => $record->id,
+				'public' => $record->public,
+				'added' => self::getDate($record->added),
+				'modified' => self::getDate($record->modified),
+				'has_derivative_image' => (bool) $record->has_derivative_image,
+				'mime_type' => $record->mime_type,
+				'order' => $record->order,
+				'size' => $record->size,
+				'stored' => (bool) $record->stored,
+				'type_os' => $record->type_os,
+				'metadata' => json_decode($record->metadata, true),
+			);
+
+
+		}
+
+        
+	$representation['item'] = array(
+            'id' => $record->item_id,
+            'url' => self::getResourceUrl("/items/{$record->item_id}"),
+            'resource' => 'items',
+        );
+        $representation['element_texts'] = $this->getElementTextRepresentations($record);
+
+        return $representation;
+    }
+
+    /**
+     * Set POST data to a file.
+     * 
+     * @param File $record
+     * @param mixed $data
+     */
+    public function setPostData(Omeka_Record_AbstractRecord $record, $data)
+    {
+        if (isset($data->order)) {
+            $record->order = $data->order;
+        }
+        $this->setElementTextData($record, $data);
+    }
+
+    /**
+     * Set PUT data to a file.
+     * 
+     * @param File $record
+     * @param mixed $data
+     */
+    public function setPutData(Omeka_Record_AbstractRecord $record, $data)
+    {
+        $this->setPostData($record, $data);
+    }
+}
